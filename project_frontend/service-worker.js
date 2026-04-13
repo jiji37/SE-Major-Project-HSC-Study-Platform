522744119819e3b93350c83905e862753f0f8f6f
jiji37 <jiya.gala954@education.nsw.gov.au>
2026-04-13 10:29:31 +1000

    populated service-worker.js

--- project_frontend/service-worker.js
+++ project_frontend/service-worker.js
@@ -1,1 +1,26 @@
+const CACHE_NAME = 'simple-pwa-cache-v1';
+const urlsToCache = [
+  '/SE-Major-Project-HSC-Study-Platform/project_frontend/index.html',
+  '/SE-Major-Project-HSC-Study-Platform/project_frontend/manifest.json',
+  '/SE-Major-Project-HSC-Study-Platform/project_frontend/main.js',
+  '/SE-Major-Project-HSC-Study-Platform/project_frontend/service-worker.js',
+  '/SE-Major-Project-HSC-Study-Platform/project_frontend/main.css,
+  '/Programming-for-the-web/simple-pwa/icon.png',
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch resources
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
 
